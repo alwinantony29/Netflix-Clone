6776f786482fa65012dcac0bc37dbbcea51810b6
alwin antony <alwinanto5885@gmail.com>
2023-02-06 17:50:20 +0530

    initial commit

---
+++ src/Components/Navbar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <img className='logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSryVEeCOGnrxzpNJyvlkMlgbNpxSHA7ciWfQ&usqp=CAU" alt="logo" />
+        
+        <button className='b1'>Sign in</button>        
+       
+    </div>
+  )
+}
+
+export default NavBar
