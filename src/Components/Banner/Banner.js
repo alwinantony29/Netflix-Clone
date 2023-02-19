a4c2b8a1e9eada809e5f462f3d6bf6e53482d8e2
alwin antony <alwinanto5885@gmail.com>
2023-02-19 18:09:24 +0530

    banner connected with tmdb

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import './Banner.css'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import axios from '../../axios'
+import { API_KEY,imageUrl } from '../../constants/constants'
+
+import './Banner.css' 
 function Banner () {
+  const [movie, setMovie] = useState([])
+  useEffect(()=>{
+    axios.get(`/trending/all/day?api_key=${API_KEY}`).then((response)=>{
+      setMovie(response.data.results[1])
+    })
+    
+  },[])
   return (
-    <div className='banner'>
+   
+    <div className='banner'  style={{ backgroundImage:`url(${movie?imageUrl+movie.backdrop_path:""})` }}>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie?movie.title : ""}</h1>
             <div className="banner_buttons">
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'>sample description</h1>
+            <h1 className='description'>{movie?movie.overview:""}</h1>
         </div>
         <div className="fade_bottom"></div>
     </div>
